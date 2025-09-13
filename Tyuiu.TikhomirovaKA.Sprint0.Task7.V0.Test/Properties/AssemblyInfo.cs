46f03d100c3c6ff7d56866c80983ed9611223f0b
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-09-13 15:17:18 +0500

    Task7

---
+++ Tyuiu.TikhomirovaKA.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TikhomirovaKA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TikhomirovaKA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("747a52fb-2b06-4a07-9a60-2276c9bbf591")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
